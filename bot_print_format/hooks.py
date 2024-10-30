d4aed852875825a28d360d243273a40b35a0d876
mazhar545 <59054130+mazhar545@users.noreply.github.com>
2024-10-30 16:57:31 +0500

    commit mazhar 30 october first

--- bot_print_format/hooks.py
+++ bot_print_format/hooks.py
@@ -242,6 +242,7 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+
 fixtures = [
     {"doctype": "Print Format", "filters": [["name", "in", ["Bot Format J", "Bot Format N","Bot Format S","Bot Format H","Bot Format B","Bot Format B Q","Bot Format HE"]]]},
     {
